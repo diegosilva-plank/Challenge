59e52c3d2028b11b070b863b51b7605b57647001
Diego-PS <diegopsilvavrb@gmail.com>
2023-05-09 09:41:05 -0300

    welcome message

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { router } from './src/routes'
 import { errors } from 'celebrate'
 import { repositoryInitialize } from './src/repositories'
+import { Request, Response } from 'express'
 
 const app = express()
 const PORT = 3333
@@ -9,6 +10,9 @@ const PORT = 3333
 app.use(express.json())
 app.use(router)
 app.use(errors());
+app.get('/', (req: Request, res: Response) => {
+    return res.json({ msg: "Welcome to my Rocket application" })
+})
 
 repositoryInitialize().then(() => {
     app.listen(PORT, () => {
