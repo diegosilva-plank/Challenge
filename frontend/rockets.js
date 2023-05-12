9d8b3a12bfa86ffc8b421720445f55688b760dd0
Diego-PS <diegopsilvavrb@gmail.com>
2023-05-12 19:04:00 -0300

    rockets page

---
+++ frontend/rockets.js
@@ -0,0 +1,54 @@
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     console.log('fechei o modal')
+//     modal.style.display = "none";
+//   }
+// }
+
+// // Get the modal
+// var modal_delete_rocket = document.getElementById("delete-rocket-modal");
+
+// // Get the button that opens the modal
+// var btn_delete_rockets = document.getElementsByClassName("delete-btn");
+
+// // Get the <span> element that closes the modal
+// var span_delete_rocket = document.getElementsByClassName("close-delete-rocket")[0];
+
+// // When the user clicks on the button, open the modal
+// for (const btn_delete_rocket of btn_delete_rockets) {
+//     btn_delete_rocket.onclick = function() {
+//         modal_delete_rocket.style.display = "block";
+//     }
+// }
+
+// // When the user clicks on <span> (x), close the modal_delete_rocket
+// span_delete_rocket.onclick = function() {
+//   modal_delete_rocket.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal_delete_rocket.style.display = "none";
+//   }
+// }
